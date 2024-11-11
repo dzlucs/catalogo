844011bde69b537a488509a234aa3fa5ad5ea7fd
dzlucas <dzlucas@live.com>
2024-11-11 19:39:46 -0300

    Commit 2 (teste) +angular +projeto

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Para arquivos HTML e JS dentro da pasta src
+    './*.html',             // Para todos os arquivos HTML na raiz do projeto
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
