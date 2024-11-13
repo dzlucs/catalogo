76be1dd699c0b4e9f428b2d921eadf06f6755ebb
dzlucas <dzlucas@live.com>
2024-11-13 15:52:07 -0300

    Branc feature at8

--- caliban/src/app/app.component.ts
+++ caliban/src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent], //add [FooterComponent, MenuComponent]
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'caliban';
 }
+
+/*@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,  // Certifique-se de que o HeaderComponent está aqui
+    FooterComponent   // Certifique-se de que o FooterComponent está aqui
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})*/
