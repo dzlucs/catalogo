63d3f002aa42b3178fa26bcaa311b938e3728adb
dzlucs <dzlucas@live.com>
2024-11-24 17:05:43 -0300

    Commit 0

--- caliban/src/app/app.component.ts
+++ caliban/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { BrowserModule } from '@angular/platform-browser';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent], //add [FooterComponent, MenuComponent]
+  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent], //add [FooterComponent, MenuComponent]
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
